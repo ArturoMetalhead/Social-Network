3bc5d608a5924402e66025fd90a2c2941a96ff50
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-07-14 16:23:46 -0400

    auto- recognized added and fixed

--- server.py
+++ server.py
@@ -7,27 +7,41 @@ import time
 
 class Server_Manager:
     
-    def __init__(self,servers,ip='localhost',port=0):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((ip,port))
+    def __init__(self,operators,twitter_servers,ip='localhost',port=0):
+        self.operator = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.operator.bind((ip,port))
         self.thread_dict={}
-        self.registered_servers = servers
-        self.sessions = {}
 
-    def start_server(self):
-        self.server.listen()
-        print(f"[*] Listening on {self.server.getsockname()}")
+
+        self.sessions = {}  
+        self.registered_twitter_servers = twitter_servers
+        self.registered_operators = operators
+        
+        self.discover_thread = None
+        self.discover_flag=False
+
+
+    def start_operator(self):
+        self.operator.listen()
+        
+        self.discover_flag=True
+        twitter_server_discover = threading.Thread(target=self.discover_twitter_servers)
+        twitter_server_discover.start()
+        operator_discover = threading.Thread(target=self.discover_operators)
+        operator_discover.start()
+        
+        print(f"[*] Listening on {self.operator.getsockname()}")
 
         while True:
-            client, addr =self.server.accept()
+            client, addr =self.operator.accept()
             print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
 
             request=client.recv(1024).decode()
 
-            if request['type'] == 'server':
-                server_handler = threading.Thread(target=self.handle_server, args=(client,))
-                server_handler.start()
-                self.thread_dict[addr[1]] = server_handler
+            if request['type'] == 'operator':
+                operator_handler = threading.Thread(target=self.handle_operator, args=(client,))
+                operator_handler.start()
+                self.thread_dict[addr[1]] = operator_handler
             
             else:
                 client_handler = threading.Thread(target=self.handle_client, args=(client,))
@@ -45,42 +59,61 @@ class Server_Manager:
         self.sessions.pop(client_socket.getpeername()[1])##########
         client_socket.close()
 
-    def discover_servers(self):
-        while True:
-            for server in self.registered_servers:
+    def discover_operators(self):
+        while True and self.discover_flag:
+            for operator in self.registered_operators:
+                ip, port = operator.split(":")
+                operator_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                operator_socket.connect((ip, port))
+                operator_socket.send(json.dumps({"action": "register"}).encode())
+                operator_socket.send(f"{self.operator.getsockname()[0]}:{self.operator.getsockname()[1]}".encode())
+                operator_socket.send(json.dumps({"action": "get_server"}).encode())
+                operator_socket.close
+            time.sleep(10)
+
+    def discover_twitter_servers(self):
+        while True and self.discover_flag:
+            for server in self.registered_twitter_servers:
                 ip, port = server.split(":")
-                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_socket.connect((ip, port))
-                server_socket.send(json.dumps({"action": "register"}).encode())
-                server_socket.send(f"{self.server.getsockname()[0]}:{self.server.getsockname()[1]}".encode())
-                server_socket.send(json.dumps({"action": "get_server"}).encode())
-                server_socket.close
+                twitter_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                twitter_socket.connect((ip, port))
+                twitter_socket.send(json.dumps({"action": "register"}).encode())
+                twitter_socket.send(f"{self.operator.getsockname()[0]}:{self.operator.getsockname()[1]}".encode())
+                twitter_socket.send(json.dumps({"action": "get_twitter_server"}).encode())
+                twitter_socket.close
             time.sleep(10)
             
 
-    def handle_server(self,server_socket):####################################
+    def handle_operator(self,operator_socket):####################################
         while True:
-            request = server_socket.recv(1024).decode()
-            if request['action'] == "get_servers":
-                server_socket.send(",".join(self.registered_servers).encode())
+            request = operator_socket.recv(1024).decode()
+            if request['action'] == "get_operators":
+                operator_socket.send(",".join(self.registered_operators).encode())
             elif request['action'] == "register":
-                server_name = server_socket.recv(1024).decode()
-                self.registered_servers.append(server_name)
-            elif request['action'] == "get_server_request":
-                servers_name = request['data']
-                for server in servers_name.split(","):
-                    if server not in self.registered_servers:
-                        self.registered_servers.append(server)
+                server_name = operator_socket.recv(1024).decode()
+                self.registered_operators.append(server_name)
+            elif request['action'] == "get_operators_request":
+                operators_name = request['data']
+                for operator in operators_name.split(","):
+                    if operator not in self.registered_operators:
+                        self.registered_operators.append(operator)
             elif request['action'] == "stop_server":
-                self.stop_server()
+                self.stop_operator()
                 break
+
+            elif request['action'] == "get_twitter_server":
+                operator_socket.send(",".join(self.registered_twitter_servers).encode())
+            elif request['action'] == "register_twitter_server":####################################
+                server_name = operator_socket.recv(1024).decode()
+                self.registered_twitter_servers.append(server_name)
 
             elif request['action'] == "send2client":
                 self.recv_and_send(request['data'],request['objetive'])
 
-
-    def stop_server(self):
-        self.server.close()
+    def stop_operator(self):
+        self.discover_flag=False
+        ################################
+        self.operator.close()
         for key in self.thread_dict:
             self.thread_dict[key].join()
 
--- twitter_server.py
+++ twitter_server.py
@@ -6,16 +6,22 @@ import json
 import time
 
 class Twitter_Server():
-    def __init__(self, servers,ip='localhost',port=0):#######
+    def __init__(self, twitter_servers,chord_node,ip='localhost',port=0):#######
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((ip,port))
         self.thread_dict={}
-        self.chord_node = None
-        self.registered_servers = servers
+        self.chord_node = chord_node
+        self.dicover_flag=False
+        self.discover_thread=None
+        self.registered_twitter_servers = twitter_servers
         self.sessions = {}
 
     def start_server(self):
         self.server.listen()
+
+        self.dicover_flag=True
+        self.discover_thread = threading.Thread(target=self.discover_twitter_servers)
+
         print(f"[*] Listening on {self.server.getsockname()}")
 
         while True:
@@ -24,7 +30,7 @@ class Twitter_Server():
             
             request=client.recv(1024).decode()
 
-            if request['type'] == 'server':
+            if request['type'] == 'twitter_server':
                 server_handler = threading.Thread(target=self.handle_server, args=(client,))
                 server_handler.start()
                 self.thread_dict[addr[1]] = server_handler
@@ -72,15 +78,15 @@ class Twitter_Server():
         self.thread_dict.pop(client_socket.getpeername()[1])
         client_socket.close()
 
-    def discover_servers(self):
-        while True:
-            for server in self.registered_servers:
+    def discover_twitter_servers(self):
+        while True and self.dicover_flag:
+            for server in self.registered_twitter_servers:
                 ip, port = server.split(":")
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket.connect((ip, port))
                 server_socket.send(json.dumps({"action": "register"}).encode())
                 server_socket.send(f"{self.server.getsockname()[0]}:{self.server.getsockname()[1]}".encode())
-                server_socket.send(json.dumps({"action": "get_server"}).encode())
+                server_socket.send(json.dumps({"action": "get_twitter_server"}).encode())
                 server_socket.close
             time.sleep(10)
 
@@ -91,22 +97,22 @@ class Twitter_Server():
         ip=server_socket.getpeername()[0]
         port=server_socket.getpeername()[1]
 
-        if f"{ip}:{port}" not in self.registered_servers:
-            self.registered_servers.append(f"{ip}:{port}")
+        if f"{ip}:{port}" not in self.registered_twitter_servers:
+            self.registered_twitter_servers.append(f"{ip}:{port}")
 
         while True:
             request = server_socket.recv(1024).decode()
-            if request['action'] == "get_server":
-                message=json.dumps({"action": "get_server_request","data":",".join(self.registered_servers)}).encode()
+            if request['action'] == "get_twitter_server":
+                message=json.dumps({"action": "get_twitter_server_request","data":",".join(self.registered_twitter_servers)}).encode()
                 server_socket.send(message)
             elif request['action'] == "register":
                 server_name = server_socket.recv(1024).decode()
-                self.registered_servers.append(server_name)
-            elif request['action'] == "get_server_request":
+                self.registered_twitter_servers.append(server_name)
+            elif request['action'] == "get_twitter_server_request":
                 servers_name = request['data']
                 for server in servers_name.split(","):
-                    if server not in self.registered_servers:
-                        self.registered_servers.append(server)
+                    if server not in self.registered_twitter_servers:
+                        self.registered_twitter_servers.append(server)
             elif request['action'] == "stop_server":
                 self.stop_server()
                 break
@@ -120,6 +126,7 @@ class Twitter_Server():
             
 
     def stop_server(self):
+        self.dicover_flag=False
         self.server.close()
         for key in self.thread_dict:
             self.thread_dict[key].join()
