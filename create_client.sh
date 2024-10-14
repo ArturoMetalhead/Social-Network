234227d2f6335a3356395b39d57c1db444a40173
Kitsulee <lauradi1410@gmail.com>
2024-10-14 03:18:50 -0400

    Deleted old testing files

--- create_client.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build -f dockerfile.client -t client-image .
-
-sudo docker run -d --name client1 --network chord-network client-image
-sudo docker run -d --name client2 --network chord-network client-image
--- create_server.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build -f dockerfile.server -t server-image .
-
-sudo docker run -d --name server1 --network chord-network server-image
-sudo docker run -d --name server2 --network chord-network server-image
